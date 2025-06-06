991bb24ca70913e70d7d2a157541ebf5c57b5e92
Victor Umekwe <flexzy4u@gmail.com>
2025-06-06 13:23:38 +0100

    added list of products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,23 @@
-
+import { sampleProducts } from "./data";
 
 const App = () => {
   return (
     <div>
       <header>Bahddest</header>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img src={product.image} alt={product.name} className="product-image"/>
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All rights reserved</footer>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
