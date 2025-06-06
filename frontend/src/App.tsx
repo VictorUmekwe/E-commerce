d82bdb14fde1b0aeac661bb5bc0fdd5da8a6a451
Victor Umekwe <flexzy4u@gmail.com>
2025-06-06 12:33:56 +0100

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+
+
+const App = () => {
+  return (
+    <div>
+      <header>Bahddest</header>
+      <main></main>
+      <footer>All rights reserved</footer>
+    </div>
+  )
+}
+
+export default App
